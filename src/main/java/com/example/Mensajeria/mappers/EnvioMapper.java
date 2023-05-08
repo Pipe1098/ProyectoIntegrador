661a573a5bc64f45483b6f154ea52b7f3a438819
pipe1098 <pipe1e@hotmail.com>
2023-05-08 12:49:43 -0500

    Corrigiendo mapper

--- src/main/java/com/example/Mensajeria/mappers/EnvioMapper.java
+++ src/main/java/com/example/Mensajeria/mappers/EnvioMapper.java
@@ -3,13 +3,13 @@ package com.example.Mensajeria.mappers;
 import com.example.Mensajeria.dto.EnvioDTO;
 import com.example.Mensajeria.model.Envio;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.factory.Mappers;
 @Mapper
 public interface EnvioMapper {
     EnvioMapper INSTANCE = Mappers.getMapper( EnvioMapper.class );
-
+    @Mapping(target = "cedula", source = "cliente.cedula")
+    @Mapping(target = "cedulaEmpleado", source = "empleado.cedula")
     EnvioDTO envioToEnvioDTO(Envio envio);
-
-   // Envio envioDTOToEnvio(EnvioDTO envioDTO);
 
 }
